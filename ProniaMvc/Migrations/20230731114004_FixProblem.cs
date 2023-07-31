1b2eaba8093314f9efaa53ad92c87b678462f22d
Adil Tehranli <adiljt@code.edu.az>
2023-07-31 15:55:29 +0400

    Update service

---
+++ ProniaMvc/Migrations/20230731114004_FixProblem.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProniaMvc.Migrations
+{
+    public partial class FixProblem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
